6e24f64fdd89c0c272e84e2d2c02054505132b45
Luana <0000726260@senaimgaluno.com.br>
2024-09-08 14:57:48 -0300

    mudança no banco de dados

--- TrabalhoAutonomo/backend/db/config.js
+++ TrabalhoAutonomo/backend/db/config.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'admin',
-    database: process.env.DB_NAME || 'plataforma_trabalho_autonomo',
+    database: process.env.DB_NAME || 'autonomeu',
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT, 10) || 3306,
     dialect: 'mysql',
